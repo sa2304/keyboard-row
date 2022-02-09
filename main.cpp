ae24c58b0b022e48272a5d3aedb2c5c04dab6266
Антон Шмидт <schmidt.ajax@gmail.com>
2022-02-09 20:02:48 +0700

    Solution

--- main.cpp
+++ main.cpp
@@ -7,9 +7,43 @@ using namespace std;
 
 class Solution {
  public:
-  vector<string> findWords(vector<string> &words) {
-    // FIXME
-    return {};
+  vector<string> findWords(vector<string>& words) {
+    vector<string> rows{
+        "qwertyuiop"s,
+        "asdfghjkl"s,
+        "zxcvbnm"s
+    };
+    for (auto& row: rows) {
+      sort(row.begin(), row.end());
+    }
+
+    vector<string> valid_words;
+    string intersection;
+    for (auto& word: words) {
+      intersection.reserve(word.length());
+      string letters = word;
+      for_each(letters.begin(), letters.end(), [](char& c) {
+        c = tolower(c);
+      });
+      sort(letters.begin(), letters.end());
+      auto u = unique(letters.begin(), letters.end());
+      letters.resize(distance(letters.begin(), u));
+
+      for (const auto& alphabet: rows) {
+        intersection.clear();
+        set_intersection(alphabet.begin(), alphabet.end(),
+                         letters.begin(), letters.end(),
+                         back_inserter(intersection));
+        if (0 < intersection.length()) {
+          if (letters.length() == intersection.length()) {
+            valid_words.push_back(word);
+          }
+          break;
+        }
+      }
+    }
+
+    return valid_words;
   }
 };
 
